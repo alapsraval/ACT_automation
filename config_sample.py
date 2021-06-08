cea922a6edc54e745adabdf469e0c19d80422659
Alap Raval <alapsraval@gmail.com>
2021-06-08 12:04:16 -0500

    config sample file

---
+++ config_sample.py
@@ -0,0 +1,10 @@
+# Encoura Service Account info. 
+# If you have not received your username, password, OrganizationId, and/or apiKey from ACT | NRCCUA, please contact support@encoura.org.
+USERNAME = "YOUR_ENCOURA_USERNAME"
+PASSWORD = "YOUR_ENCOURA_PASSWORD"
+ORGANIZATION_UID = "YOUR_ENCOURA_UID"
+API_KEY = "YOUR_ENCOURA_APIKEY"
+
+# Your Technolutions FTP login credentials. Make sure you whitelist the ip address where you will be running the script from.
+FTP_USERNAME = "YOUR_SLATE_FTP_USERNAME"
+FTP_PASSWORD = "YOUR_SLATE_FTP_PASSWORD"
